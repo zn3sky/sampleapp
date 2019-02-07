093d794e666c84315962aa8fd4a25d9c0448f46d
zn3sky <zn3sky@gmail.com>
2019-02-07 21:31:18 +0900

    Initialize repository

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Hello Vue</title>
+    <link rel="stylesheet" href="css/app.css"/>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script type="text/javascript">
+        window.Laravel = window.Laravel || {};
+        window.Laravel.csrfToken = "{{csrf_token()}}";
+    </script>
+</head>
+<body>
+<div id="app">
+    <example-component></example-component>
+</div>
+<script src="js/app.js"></script>
+</body>
+</html>
